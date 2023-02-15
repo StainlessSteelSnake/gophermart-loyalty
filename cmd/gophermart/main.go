4bef58b7f8769a635f01043e0189525cb4d6780d
StainlessSteelSnake <stainlesssteelsnake@gmail.com>
2023-02-15 18:42:48 +0300

    Update main.go

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/StainlessSteelSnake/gophermart-loyalty/internal/config"
+import (
+	"context"
+	"github.com/StainlessSteelSnake/gophermart-loyalty/internal/config"
+	"github.com/StainlessSteelSnake/gophermart-loyalty/internal/database"
+	"log"
+)
 
 func main() {
 	cfg := config.NewConfiguration()
 	if cfg == nil {
+		log.Fatal("Не удалось получить конфигурацию сервиса системы лояльности")
+	}
+
+	ctx, _ := context.WithCancel(context.Background())
+	dbStorage := database.NewDatabaseStorage(ctx, cfg.DatabaseURI)
+	if dbStorage == nil {
+		log.Fatal("Не удалось инициализировать БД сервиса системы лояльности")
 	}
 }
